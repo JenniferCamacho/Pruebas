dff1c61968bb285e478a595643a3e861442ab541
JenniferCamacho <jennifercamacho9810@gmail.com>
2022-03-23 11:23:20 -0500

    Basico phython

---
+++ Python/prueba1.py
@@ -0,0 +1,22 @@
+# from numbers import Real
+# from operator import truediv
+# from xmlrpc.client import boolean
+
+
+# print('Hola Mundo')
+# entero=5
+# booleano= True
+# real=4.5
+# print (type(real))
+
+# #división solo entero
+# div2=2//10
+# print(div2)
+# # residuo
+# modulo=8%5
+# print(modulo)
+
+edad=input('Digite la edad ')
+print(edad)
+# str,int 
+entero= int(edad)
