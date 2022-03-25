d927c44945a299040b7de90dcc76bee7849831be
JenniferCamacho <jennifercamacho9810@gmail.com>
2022-03-25 15:47:48 -0500

    clase de python

--- Python/prueba1.py
+++ Python/prueba1.py
@@ -23,13 +23,58 @@
 
 # frase=('I don\'t know')
 # frase=('universidad\tSanto\nTomas')
-frase=('usta')
-# # primer caracter
-# print(frase[0]) 
-# # último caracter
-# print(frase[-1])
-# # longitud
-print(len(frase))
-print(frase[0:2])
+# frase=('usta')
+# # # primer caracter
+# # print(frase[0]) 
+# # # último caracter
+# # print(frase[-1])
+# # # longitud
+# print(len(frase))
+# print(frase[0:2])
+# encontrar una letra o cadena de caracteres
+# esta= 'a' in frase
+# print (esta)}
 
+# template
+# name='Jennifer'
+# age=20
+# mensaje=f'La estudiante {name} tiene {age} años'
+# print (mensaje)
 
+# # contar caracteres
+# letra= 'n'
+# cuantos=name.count(letra)
+# print (f'{name} tiene {cuantos} letras {letra}')
+
+x=4
+if x==3:
+    print('el numero es 3')
+else:
+    print('No es 3')
+
+y=20
+if y>25:
+    print('es menor a 10')
+elif y<5:
+    print('es menor a 5')
+elif y<3:
+    print('es menor a 3')
+else:
+    print('es dos o uno 0 cero')
+
+i=0
+while i<5:
+    print(i)
+    i=i+1
+
+print('$'*10)
+
+# imprime del 5 al 20 sin incluir el 20 y de 3 en 3
+for numero in range(5,20,3):
+    print(numero)
+
+numeros=[]
+numeros1=[1,2,3,4,5]
+print(numeros1)
+
+# c.pop/ c.remove/ c.reverse...
