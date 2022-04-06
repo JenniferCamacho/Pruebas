dd128fa5c815fd63d588b9121d87deb8b37bff45
JenniferCamacho <jennifercamacho9810@gmail.com>
2022-04-06 12:01:54 -0500

    bases de datos

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre=models.CharField(max_length=100, null=False)
+    descripcion=models.TextField(null=True)
+
+    class Meta:
+        app_label='app'
+        
